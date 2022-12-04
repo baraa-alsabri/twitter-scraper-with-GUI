176361128cf684680fbde8c9f9b60c6dd0a34ded
baraa-alsabri <baraa.s.alsabri@gmail.com>
2022-12-04 18:45:09 +0300

    First Version

---
+++ main.py
@@ -0,0 +1,162 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter import filedialog
+from threading import Thread
+from tkcalendar import Calendar
+from datetime import datetime
+
+from scrapper import ScrapeTweets
+
+
+window = Tk()
+window.title('Tweets Scraper')
+window.config(pady=20,padx=20)
+window.resizable(False,False) # to disable the minimize/maximize buttons. 
+
+
+#Username.
+website_label = Label(text='Username:')
+website_label.grid(column=0,row=1)
+
+username_entry = Entry(width=35)
+username_entry.focus() # to make the typer starts in the website entry when we start the program.
+username_entry.insert(0,'@')
+username_entry.grid(column=1,row=1,columnspan=2)
+
+#Email.
+email_label = Label(text='Email:')
+email_label.grid(column=0,row=2)
+
+email_entry = Entry(width=35)
+                        
+email_entry.grid(column=1,row=2,columnspan=2)
+
+
+#Password.
+Password_label = Label(text='Password:')
+Password_label.grid(column=0,row=3)
+
+Password_entry = Entry(show="*" , width=21)
+Password_entry.grid(column=1,row=3)
+
+
+#hashtags.
+hashtags_label = Label(text='Hashtag(s):')
+hashtags_label.grid(column=0,row=4)
+hashtags_entry = Entry(width=50)
+hashtags_entry.grid(column=1,row=4)
+
+#Web driver path.
+chrome_webdriver_path = None
+
+def get_webdriver_path():
+    global chrome_webdriver_path
+    chrome_webdriver_path_fd = filedialog.askopenfilename(title='Select Chrome WebDriver')
+    chrome_webdriver_path = chrome_webdriver_path_fd
+
+
+chrome_webdriver_path_label = Label(text='Chrome Web Driver Path:')
+chrome_webdriver_path_label.grid(column=0,row=5)
+chrome_webdriver_path_button = Button(text='Browse' ,command=get_webdriver_path)
+chrome_webdriver_path_button.grid(column=1,row=5)
+
+
+from_date = None
+to_date = None
+
+def start_scraping():
+    global chrome_webdriver_path
+
+    global from_date
+    global to_date
+
+    username = username_entry.get()
+    email = email_entry.get()
+    password = Password_entry.get()
+    hashtags = hashtags_entry.get()
+
+    if username == '' or email == '' or password == '' or hashtags == '' or chrome_webdriver_path == None:
+        messagebox.showerror('Missing failed(s)' , 'Be Sure to Fill Out all the Faileds and choese chrome WebDriver.')
+    else:    
+        if to_date and from_date:
+            if to_date < from_date or to_date == from_date:
+                messagebox.showerror('Dates Error' , 'The Start Date must be before the End Date ,also they can not be equivalent.')
+    
+        if from_date == None:
+            from_date = datetime(year=2006 , month= 1 , day= 1).date()
+        if to_date == None:
+            to_date = datetime.today().date()
+
+        while True:
+            scraper = ScrapeTweets(
+                username = username,
+                email = email,
+                password = password,
+                hashtags = hashtags, 
+                chrome_webdriver_path = chrome_webdriver_path,
+                from_date = from_date,
+                to_date = to_date)
+            try:
+                scraper.login()         
+                break
+            except Exception  as e:
+                messagebox.showerror(title='Login Failed click Ok to try again or check your internet connection',message=f'{e}')
+                scraper.driver.quit()
+
+        
+        scraper.search()
+        
+
+
+def set_from_date():
+    def print_sel():
+        global from_date
+        from_date = cal.selection_get()
+        from_date_label.config(text = f'Sat To {cal.selection_get()}')
+        top.destroy()
+        top.update()
+
+    top = Toplevel(window)
+
+    cal = Calendar(top,
+                   font="Arial 14", selectmode='day',
+                   cursor="hand1", year=2018, month=2, day=5)
+    cal.grid(row= 0 ,column = 0)
+    Button(top, text="Set", command=print_sel).grid(row=1 , column=1)
+
+def set_to_date():
+    def print_sel():
+        global to_date
+        to_date = cal.selection_get()
+        to_date_label.config(text = f'Sat To {cal.selection_get()}')
+        top.destroy()
+        top.update()
+
+    top = Toplevel(window)
+
+    cal = Calendar(top,
+                   font="Arial 14", selectmode='day',
+                   cursor="hand1", year=2018, month=2, day=5)
+    cal.grid(row= 0 ,column = 0)
+    Button(top, text="Set", command=print_sel).grid(row=1 , column=1)
+
+
+def start_scraping_thread():
+    Thread(target = start_scraping).start()
+
+
+from_date_label = Label(text='From Date (Optional)')
+from_date_label.grid(column=0,row=7)
+Button(window, text='Set', command=set_from_date).grid(column=2, row=7)
+
+to_date_label = Label(text='To Date (Optional)')
+to_date_label.grid(column=0,row=8)
+Button(window, text='Set', command=set_to_date).grid(column=2, row=8)
+
+
+start_button = Button(text = 'Start' , command=start_scraping_thread)
+start_button.grid(column=1,row=10)
+
+
+window.mainloop()
+
---
+++ scrapper.py
@@ -0,0 +1,231 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+import pandas as pd
+import re
+from datetime import datetime , timedelta
+from time import sleep
+from random import randint
+
+
+
+class ScrapeTweets:
+    def __init__(self, username, email , password ,  hashtags , chrome_webdriver_path , from_date, to_date):
+        self.username = username
+        self.email = email
+        self.password = password
+        
+        self.hashtags = hashtags.replace('\n','') # Clear self.search_query from any unexpacted chars
+        
+        delta:timedelta = to_date - from_date
+
+        self.search_range = []
+
+        for i in range(delta.days + 1):
+            self.search_range.append(from_date + timedelta(days=i))
+
+        self.search_range.reverse() # to start searching from newest to oldest
+        
+        self.tweets_buffer = []
+
+
+        options = webdriver.ChromeOptions()
+        options.add_argument('--start-maximized')
+        options.add_argument('--disable-gpu')
+        options.add_argument("--disable-notifications")
+
+
+        csv_file = pd.DataFrame({
+                    'username': [],
+                    'handle': [],
+                    'date': [],
+                    'text': [],
+                    'hashtags': [],
+                    'reply_count': [],
+                    'retweet_count': [],
+                    'like_count': [],
+                            })
+        
+        try:
+            self.data_file_name = self.hashtags
+            csv_file.to_csv(f'{self.data_file_name}.csv' , index=False)
+        except OSError: # Sometimes hashtag's names may contain chars that aren't allowed as file name.
+            self.data_file_name = randint(10000,1000000)
+            csv_file.to_csv(f'{self.data_file_name}.csv' , index=False)       
+
+        
+        self.driver = webdriver.Chrome(chrome_webdriver_path , options=options)
+        self.driver.implicitly_wait(30)
+
+        self.driver.get('https://twitter.com/login')
+
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}(Data Entries={len(self.df)})'
+
+    def login(self):
+        print('Logging in')
+        
+        # Username
+        login_email_input = self.driver.find_element(By.XPATH,"//input[@name='text']")
+        login_email_input.send_keys(self.email)
+        next_button = self.driver.find_element(By.XPATH,"//span[contains(text(),'Next')]")
+        next_button.click()
+
+        
+        # If verfcation needed
+        visable_spans = self.driver.find_elements(By.TAG_NAME , 'span')
+        visable_texts = []
+        for text in visable_spans:
+            visable_texts.append(text.text)
+
+        if 'There was unusual login activity on your account. To help keep your account safe, please enter your phone number or username to verify it’s you.' in visable_texts:
+            username_verfcation_input = self.driver.find_element(By.TAG_NAME , 'input')
+            username_verfcation_input.send_keys(self.username)
+            username_verfcation_input.send_keys(Keys.RETURN)
+            
+
+        
+        # Password
+        login_password_input = self.driver.find_element(By.XPATH,"//input[@name='password']")
+        login_password_input.send_keys(self.password)
+        log_in = self.driver.find_element(By.XPATH,"//span[contains(text(),'Log in')]")
+        log_in.click()
+    
+        print(f'[*] Logged In As {self.username}\n')
+
+
+
+    def search(self):
+        for date in self.search_range:
+            query_from_date = date - timedelta(days=1)
+            query_to_date = date
+
+            if query_from_date.month < 10:
+                from_month = f'0{query_from_date.month}'
+            else:
+                from_month = f'{query_from_date.month}'
+            
+            if query_from_date.day < 10:
+                from_day = f'0{query_from_date.day}'
+            else:
+                from_day = f'{query_from_date.day}'
+
+
+            if query_to_date.month < 10:
+                to_month = f'0{query_to_date.month}'
+            else:
+                to_month = f'{query_to_date.month}'
+            
+            if query_to_date.day < 10:
+                to_day = f'0{query_to_date.day}'
+            else:
+                to_day = f'{query_to_date.day}'
+            
+            self.search_query = f'{self.hashtags} until:{query_to_date.year}-{to_month}-{to_day} since:{query_from_date.year}-{from_month}-{from_day}'
+
+            search_input = self.driver.find_element(By.XPATH , '//input[@aria-label="Search query"]')
+            search_input.send_keys(Keys.CONTROL + 'a')
+            search_input.send_keys(Keys.DELETE)
+
+            search_input.send_keys(self.search_query)
+            search_input.send_keys(Keys.RETURN) # Press Enter
+
+            self.driver.execute_script("document.body.style.zoom='25%'") # Zoom out the browser to show as much tweets as possible
+
+            sleep(5)
+            self.scroll()
+
+
+    def get_tweet_data(self , card): 
+        """Extract data from tweet card"""
+        username = card.find_element(By.XPATH,'.//span').text
+
+        try:
+            handle = card.find_element(By.XPATH,'.//span[contains(text(), "@")]').text
+        except NoSuchElementException:
+            handle = ''
+        
+        try:
+            postdate_and_time = card.find_element(By.XPATH,'.//time').get_attribute('datetime')
+        except NoSuchElementException:
+            postdate_and_time = ''
+        
+        text = ''
+        hashtags = ''
+
+
+        tweet_content = card.find_element(By.XPATH,'.//div[@data-testid="tweetText"]')
+        
+        tweet_text = tweet_content.find_elements(By.TAG_NAME , 'span')
+        for word in tweet_text:
+            text += word.text
+
+        if text == '': 
+            return 'There is no tweet'
+
+        tweet_hashtags = tweet_content.find_elements(By.TAG_NAME , 'a')
+        for hashtag in tweet_hashtags:
+            if '#' in hashtag.text: #to excilude any other links
+                hashtags += f'{hashtag.text}\n'
+
+
+        reply_count = card.find_element(By.XPATH,'.//div[@data-testid="reply"]').text
+        retweet_count = card.find_element(By.XPATH,'.//div[@data-testid="retweet"]').text
+        like_count = card.find_element(By.XPATH,'.//div[@data-testid="like"]').text
+        
+        
+        tweet = [username, handle, postdate_and_time ,text ,hashtags, reply_count, retweet_count, like_count]
+        
+        if tweet not in self.tweets_buffer:
+            self.tweets_buffer.append(tweet) 
+        
+
+    def scroll(self):
+        # get all tweets on the page
+        last_position = self.driver.execute_script("return window.pageYOffset;")
+        scrolling = True
+
+        sleep(2)
+
+        while scrolling:
+            page_cards = self.driver.find_elements(By.CSS_SELECTOR, '[data-testid="tweet"]')
+
+            for card in page_cards[-15:]:
+                self.get_tweet_data(card)
+                    
+            scroll_attempt = 0
+            while True:
+                self.driver.execute_script('window.scrollBy(0, 800);')
+                sleep(2)
+                curr_position = self.driver.execute_script("return window.pageYOffset;")
+                if last_position == curr_position:
+                    scroll_attempt += 1
+                    # end of scroll region
+                    if scroll_attempt >= 3: 
+                        scrolling = False
+                        self.save_tweet_to_csv()
+                        break
+                    else:
+                        sleep(2) # attempt another scroll
+                else:
+                    last_position = curr_position
+                    break
+
+
+    def save_tweet_to_csv(self):
+        for tweet in self.tweets_buffer:
+            df = pd.DataFrame({
+                    'username': [tweet[0]],
+                    'handle': [tweet[1]],
+                    # Convert date and time from string to datetime object i.e '2022-11-28T23:59:09.000Z'
+                    'date': datetime.strptime(tweet[2].replace('Z','') , '%Y-%m-%dT%H:%M:%S.%f'),
+                    'text': [tweet[3]],
+                    'hashtags': [tweet[4]],
+                    'reply_count': [tweet[5]],
+                    'retweet_count': [tweet[6]],
+                    'like_count': [tweet[7]]
+                })
+            df.to_csv(f'{self.data_file_name}.csv', mode='a',index=False ,header=False)
